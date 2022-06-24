d3fce351688408edfa4fae8a908b6f73dd8c2596
QuangNLee <lengocquang8196@gmail.com>
2022-06-24 10:47:36 +0700

    update layout admin

--- inc/header.php
+++ inc/header.php
@@ -27,7 +27,7 @@
 ?>
 <!DOCTYPE HTML>
 <head>
-    <title>Online Store BD</title>
+    <title>Store Online BD</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
